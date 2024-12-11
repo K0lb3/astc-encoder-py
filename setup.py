488da083b0006b237d07fbd12c51cee0fac1eb2b
Rudolf Kolbe <rkolbe96@gmail.com>
2024-12-11 11:37:56 +0100

    0.1.11 - add /D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR flag for msvc

--- setup.py
+++ setup.py
@@ -124,6 +124,8 @@ class CustomBuildExt(build_ext):
                 "/EHsc",
                 "/W4",
                 "/fp:precise",
+                # msvc runtime version incompatibility fix
+                "/D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR",
             ]
             # not in the astc-encoder CMakeLists.txt
             # even tho it should improve performance
